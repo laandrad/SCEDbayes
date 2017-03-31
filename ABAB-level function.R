5366110a81634e825620e2881d6d860b03c8dd1d
mhanauer <mhanauer@indiana.edu>
2017-03-31 15:04:54 -0400

    Update ABAB-level function.R

--- ABAB-level function.R
+++ ABAB-level function.R
@@ -1,135 +1,175 @@
-# JAGS stands for Just Another Gibbs Sampler 
-# JAGS builds MCMC sampler 
-# JAGS takes a user's defined model and returns an MCMC sample of posterior distribution 
-# To use the ABJags fuction, users first need to install JAGS and rjags 
-# JAGS can be downloaded from http://mcmc-jags.sourceforge.net/
+# Example for Jags-Ymet-XmetSsubj-MrobustHier.R 
+#------------------------------------------------------------------------------- 
+# Optional generic preliminaries:
+graphics.off() # This closes all of R's graphics windows.
+rm(list=ls())  # Careful! This clears all of R's memory!
+#------------------------------------------------------------------------------- 
+# Load data file and specity column names of x (predictor) and y (predicted):
 
-ABABJags = function(y,P){
-  if(!require(rjags)){
-    install.packages("rjags")
-    library(rjags)
-  }
+myData = read.csv( file="Lambert.csv" )
+myData = cbind( myData , X3 = myData[,"X"] * myData[,"X2"] )
+xName = "X" ; x2Name = "X2"; x3Name = "X3"; yName = "Y" ; sName="Subj"
+fileNameRoot = "HierLinRegressData-Jags-" 
+
+graphFileType = "eps" 
+#------------------------------------------------------------------------------- 
+# Load the relevant model into R's working memory:
+source("Jags-Ymet-XmetSsubj-MrobustHierExtendSCED.R")
+#------------------------------------------------------------------------------- 
+# Generate the MCMC chain:
+#startTime = proc.time()
+mcmcCoda = genMCMC( data=myData , xName=xName ,x2Name = x2Name,x3Name = x3Name, yName=yName , sName=sName ,
+                    numSavedSteps=20000 , thinSteps=15 , saveName=fileNameRoot )
+
+#------------------------------------------------------------------------------- 
+# Get summary statistics of chain:
+summaryInfo = smryMCMC( mcmcCoda , saveName=fileNameRoot )
+show(summaryInfo)
+
+###################################################################################################################################################
+# Start low level script
+# Jags-Ymet-XmetSsubj-MrobustHier.R 
+# Accompanies the book:
+#  Kruschke, J. K. (2015). Doing Bayesian Data Analysis, Second Edition: 
+#  A Tutorial with R, JAGS, and Stan. Academic Press / Elsevier.
+# This has the most updated version but won't work
+source("DBDA2E-utilities.R")
+
+#===============================================================================
+
+genMCMC = function( data , xName="x" , x2Name = "x2",x3Name = "x3", yName="y" , sName="s" ,
+                    numSavedSteps=10000 , thinSteps = 1 , saveName=NULL ,
+                    runjagsMethod=runjagsMethodDefault , 
+                    nChains=nChainsDefault) { 
   
-  model.s = "
+  #-----------------------------------------------------------------------------
+  # THE DATA.
+  y = data[,yName]
+  x = data[,xName]
+  x2 = data[,x2Name]
+  x3 = data[,x3Name]
+  # Convert sName to consecutive integers:
+  s = as.numeric(factor(data[,sName]))
+  # Do some checking that data make sense:
+  if ( any( !is.finite(y) ) ) { stop("All y values must be finite.") }
+  if ( any( !is.finite(x) ) ) { stop("All x values must be finite.") }
+  #Ntotal = length(y)
+  # Specify the data in a list, for later shipment to JAGS:
+  dataList = list(
+    x = x ,
+    x2 = x2,
+    x3 = x3,
+    y = y ,
+    s = s ,
+    Nsubj = max(s)  # should equal length(unique(s))
+  )
+  #-----------------------------------------------------------------------------
+  # THE MODEL.
+  modelString = "
+  # Standardize the data:
+  data {
+  Ntotal <- length(y)
+  xm <- mean(x)
+  x2m <- mean(x2)
+  x3m <- mean(x3)
+  ym <- mean(y)
+  xsd <- sd(x)
+  x2sd <- sd(x2)
+  x3sd <- sd(x3)
+  ysd <- sd(y)
+  for ( i in 1:length(y) ) {
+  zx[i] <- ( x[i] - xm ) / xsd
+  zx2[i] <- ( x2[i] - x2m ) / x2sd
+  zx3[i] <- ( x3[i] - x3m ) / x3sd
+  zy[i] <- ( y[i] - ym ) / ysd
+  }
+  }
+  # Specify the model for standardized data:
   model {
-  for(i in 1:nPoints){
-  y[i] ~ dnorm(mu[i], tau)    
-  mu[i] <- beta0 + beta1 * P1[i] + beta2 * P2[i] + beta3 * P3[i]
+  for ( i in 1:Ntotal ) {
+  zy[i] ~ dt( zbeta0[s[i]] + zbeta1[s[i]] * zx[i] + zbeta2[s[i]] * zx2[i] + zbeta3[s[i]] * zx3[i], 1/zsigma^2 , nu )
   }
-  beta0 ~ dnorm(0, 0.001)     
-  beta1 ~ dnorm(0, 0.001)
-  beta2 ~ dnorm(0, 0.001)     
-  beta3 ~ dnorm(0, 0.001)
-  tau ~ dgamma(0.001, 0.001)
+  for ( j in 1:Nsubj ) {
+  zbeta0[j] ~ dnorm( zbeta0mu , 1/(zbeta0sigma)^2 )  
+  zbeta1[j] ~ dnorm( zbeta1mu , 1/(zbeta1sigma)^2 )
+  zbeta2[j] ~ dnorm( zbeta2mu , 1/(zbeta2sigma)^2 )
+  zbeta3[j] ~ dnorm( zbeta3mu , 1/(zbeta3sigma)^2 )
+  
+  
   }
-  "
-  writeLines(model.s, con='model.txt')
+  # Priors vague on standardized scale:
+  zbeta0mu ~ dnorm( 0 , 1/(10)^2 )
+  zbeta1mu ~ dnorm( 0 , 1/(10)^2 )
+  zbeta2mu ~ dnorm( 0 , 1/(10)^2 )
+  zbeta3mu ~ dnorm( 0 , 1/(10)^2 )
+  zsigma ~ dnorm( 1.0E-3 , 1.0E+3 )
+  zbeta0sigma ~ dunif( 1.0E-3 , 1.0E+3 )
+  zbeta1sigma ~ dunif( 1.0E-3 , 1.0E+3 )
+  zbeta2sigma ~ dunif( 1.0E-3 , 1.0E+3 )
+  zbeta3sigma ~ dunif( 1.0E-3 , 1.0E+3 )
   
-  # create dummy ABAB variables and Time according to Moeyaert et al. (2014)
-  phase = P
-  x = vector()
-  for(i in 2:length(phase)) x = c(x, phase[i]-phase[i-1])
-  nPhase = which(!x==0)
-  P1 = c(rep(0, nPhase[1]), rep(1, length(phase) - nPhase[1]))
-  P2 = c(rep(0, nPhase[2]), rep(1, length(phase) - nPhase[2]))
-  P3 = c(rep(0, nPhase[3]), rep(1, length(phase) - nPhase[3]))
-  y = y
-  nPoints = length(y)
-  t = 1:nPoints
-
-  data = list(
-    nPoints = nPoints,
-    y = y,
-    P1 = P1,
-    P2 = P2,
-    P3 = P3
-  )
-  
-  # Initialize the chains with Maximum Likelihood estimators
-  reg = lm(y ~ P1 + P2 + P3)      #fit linear model
-  
-  initsList = list(
-    beta0 = reg$coefficients[1] ,    
-    beta1 = reg$coefficients[2] ,       
-    beta2 = reg$coefficients[3] ,    
-    beta3 = reg$coefficients[4] ,       
-    tau = length(y) / sum(reg$residuals^2)  
-  )
-  
-  param = c('beta0', 'beta1', 'beta2', 'beta3', 'tau') # Specify Bayesian estimates to be extracted
-  adaptSteps = 1000
-  burnInSteps = 1000
-  nChains = 3
-  numSavedSteps = 20000
-  thinSteps = 1
-  nIter = ceiling((numSavedSteps * thinSteps)/nChains)
-  
-  # Package everything to be sent to JAGS
-  jagsModel = jags.model('model.txt', data=data, n.chains = nChains, n.adapt = adaptSteps, inits=initsList)
-  
-  # Start the chains and get rid of first n steps
-  cat('Burning in the MCMC chain...\n')
-  update(jagsModel, n.iter=burnInSteps)
-  
-  # Get the tunned chains
-  cat('Sampling final MCMC chain...\n')
-  codaSamples = coda.samples(jagsModel, variable.names=param, n.iter=nIter, thin=thinSteps)
-  
-  summary(codaSamples)
-  
-  # Save the sampled chains as a matrix for further processing
-  mcmcChain = as.matrix(codaSamples)
-  parameter = colnames(mcmcChain)
-
-  # Graphing the results
-  openGraph = function( width=7 , height=7 , ... ) {
-    if ( .Platform$OS.type != "windows" ) { # Mac OS, Linux
-      X11( width=width , height=height , type="cairo" , ... ) 
-    } else { # Windows OS
-      windows( width=width , height=height , ... )
-    }
+  nu ~ dexp(1/30.0)
+  # Transform to original scale:
+  for ( j in 1:Nsubj ) {
+  beta1[j] <- zbeta1[j] * ysd / xsd 
+  beta2[j] <- zbeta2[j] * ysd / x2sd
+  beta3[j] <- zbeta3[j] * ysd / x3sd
+  beta0[j] <- zbeta0[j] * ysd  + ym - zbeta1[j] * xm * ysd / xsd + zbeta2[j] * x2m * ysd / x2sd + zbeta3[j] * x3m * ysd / x3sd
   }
-
-  openGraph(width = 14, height = 7)
-  layout(matrix(c(1:4,rep(5,4)), nrow = 2, byrow=T))
-
-  plot.titles = c('A1 Level', 'B1-A1 Level Difference',
-                  'A2-B1 Level Difference', 'B2-A2 Level Difference')
-  
-  mu = apply(mcmcChain, 2, mean)
-  min.v = apply(mcmcChain, 2, min)
-  max.v = apply(mcmcChain, 2, max)
-  HDI = t(apply(mcmcChain, 2, function(x) quantile(x, probs = c(0.025,0.975))))
-  holder = apply(mcmcChain, 2, function(x) max(density(x)$y) / 6)
-  
-  for(i in 1:4){
-    CD0 = HDI[i,]>0
-    CD0 = ifelse(CD0[1]==CD0[2], '*** credibly not 0', '+ credibly 0')
-    hdi.coords = xy.coords(HDI[i,], rep(holder[i],2))
-    hist(mcmcChain[,i], main=plot.titles[i], lwd=2, bty='n', probability = T, axes = F,
-         sub = CD0, xlab='', ylab='', col='light green', border = 'white')
-    axis(1, c(min.v, max.v), tck=0, labels = F)
-    abline(v=0, lty = 2, col='red')
-    text(0, holder[i]*1.5, '0')
-    lines(hdi.coords, lwd=3, col='dark green')
-    text(HDI[i,1],holder[i]*1.5,as.character(round(HDI[i,1],2)))
-    text(HDI[i,2],holder[i]*1.5,as.character(round(HDI[i,2],2)))
-    text(mu[i],holder[i]*5,bquote(paste(mu[.(parameter[i])],' = ',.(mu[i]))))
+  beta1mu <- zbeta1mu * ysd / xsd
+  beta2mu <- zbeta2mu * ysd / x2sd
+  beta3mu <- zbeta3mu * ysd / x3sd
+  beta0mu <- zbeta0mu * ysd  + ym - zbeta1mu * xm * ysd / xsd + zbeta2mu * x2m * ysd / x2sd + zbeta3mu * x3m * ysd / x3sd
+  sigma <- zsigma * ysd
   }
+  " # close quote for modelString
+  # Write out modelString to a text file
+  writeLines( modelString , con="TEMPmodel.txt" )
+  #-----------------------------------------------------------------------------
+  # INTIALIZE THE CHAINS.
+  # Let JAGS do it...
+  #-----------------------------------------------------------------------------
+  # RUN THE CHAINS
+  parameters = c( "beta0" ,  "beta1","beta2" , "beta3", "beta0mu" , "beta1mu" , "beta2mu", "beta3mu",
+                  "zbeta0" , "zbeta1" , "zbeta2", "zbeta3",  "zbeta0mu" , "zbeta1mu" ,"zbeta2mu", "zbeta3mu",
+                  "zsigma", "sigma", "nu" , "zbeta0sigma" , "zbeta1sigma","zbeta2sigma", "zbeta3sigma")
+  adaptSteps = 1000  # Number of steps to "tune" the samplers
+  burnInSteps = 2000
+  runJagsOut <- run.jags( method=runjagsMethod ,
+                          model="TEMPmodel.txt" , 
+                          monitor=parameters , 
+                          data=dataList ,  
+                          #inits=initsList , 
+                          n.chains=nChains ,
+                          adapt=adaptSteps ,
+                          burnin=burnInSteps , 
+                          sample=ceiling(numSavedSteps/nChains) ,
+                          thin=thinSteps ,
+                          summarise=FALSE ,
+                          plots=FALSE )
+  codaSamples = as.mcmc.list( runJagsOut )
+  # resulting codaSamples object has these indices: 
+  #   codaSamples[[ chainIdx ]][ stepIdx , paramIdx ]
   
-  plot(y~t, main='Level Comparisons', pch=16, type='b', col='dark green', frame.plot=F)
-  for(i in 1:3) abline(v=nPhase[i]+.5, lty = 2)
-  nPhases = c(1, nPhase, length(t))
-  phase.level = round(cumsum(mu),2)
-  phases = c('A1', 'B1', 'A2', 'B2')
-  mid.x = c(nPhases[1:4]+(nPhases[2:5]-nPhases[1:4])/2)
-  text(mid.x, rep(max(y),4), phases, cex = 2)
-  for(i in 1:4) lines(c(nPhases[i]+.5, nPhases[i+1]+.5), rep(phase.level[i],2), lty = 2, col='red')
-  for(i in 1:4) text(nPhases[i]+1, phase.level[i], bquote(paste(mu[.(phases[i])],' = ',.(phase.level[i]))), adj=c(-1,1))
-  
-  results.table = data.frame(parameter[1:length(mu)], mu, HDI)
-  colnames(results.table) = c('Parameter', 'Estimate', 'Lower.HDI', 'Higher.HDI')
+  if ( !is.null(saveName) ) {
+    save( codaSamples , file=paste(saveName,"Mcmc.Rdata",sep="") )
+  }
+  return( codaSamples )
+} # end function
 
-  return(results.table)
-  
+#===============================================================================
+
+smryMCMC = function(  codaSamples , 
+                      saveName=NULL ) {
+  mcmcMat = as.matrix(codaSamples,chains=FALSE)
+  paramNames = colnames(mcmcMat)
+  summaryInfo = NULL
+  for ( pName in paramNames ) {
+    summaryInfo = rbind( summaryInfo ,  summarizePost( mcmcMat[,pName] ) )
+  }
+  rownames(summaryInfo) = paramNames
+  if ( !is.null(saveName) ) {
+    write.csv( summaryInfo , file=paste(saveName,"SummaryInfo.csv",sep="") )
+  }
+  return( summaryInfo )
 }
