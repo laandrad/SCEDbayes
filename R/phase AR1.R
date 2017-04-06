61c6cc25ef909387e94bef786969b8ff206e75bc
Alejandro Andrade <landrad78@gmail.com>
2017-04-06 13:18:49 -0600

    Update phase AR1.R

--- R/phase AR1.R
+++ R/phase AR1.R
@@ -5,12 +5,13 @@ phase.AR1 <- function(bayes.coeff) {
   N = nrow(bayes.coeff)
 
   # sapply(1:N, function(j) print((beta0[j] + beta1[j])/(1-rho[j])))
-  gamma0 = beta0                                                                            ## A1 level
-  gamma1 = sapply(1:N, function(j) (beta0[j] + beta1[j])/(1-rho[j]))                        ## B1 level
-  gamma2 = sapply(1:N, function(j) (beta0[j] + beta1[j]+ beta2[j])/(1-rho[j]))              ## A2 level
-  gamma3 = sapply(1:N, function(j) (beta0[j] + beta1[j] + beta2[j] + beta3[j])/(1-rho[j]))  ## B2 level
-
-  return(data.frame(gamma0, gamma1, gamma2, gamma3))
+  beta0A1 = beta0                                                                            ## A1 level
+  beta0B1 = sapply(1:N, function(j) (beta0[j] + beta1[j])/(1-rho[j]))                        ## B1 level
+  beta0A2 = sapply(1:N, function(j) (beta0[j] + beta1[j]+ beta2[j])/(1-rho[j]))              ## A2 level
+  beta0B2 = sapply(1:N, function(j) (beta0[j] + beta1[j] + beta2[j] + beta3[j])/(1-rho[j]))  ## B2 level
+  sigma =   sapply(1:N, function(j) (sigma.delta[j]/(1-rho[j])))                                 ## stdev
+  
+  return(data.frame(beta0A1, beta0B1, beta0A2, beta0B2, sigma))
 
 }
 
