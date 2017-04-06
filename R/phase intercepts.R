1f8975c0e64429b9cc81f83a2895266c9764911c
Alejandro Andrade <landrad78@gmail.com>
2017-04-06 13:19:28 -0600

    Update phase intercepts.R

--- R/phase intercepts.R
+++ R/phase intercepts.R
@@ -4,14 +4,12 @@ phase.levels <- function(bayes.coeff) {
 
   N = nrow(bayes.coeff)
 
-  gamma0 = beta0                                                               ## A1 level
-  gamma1 = sapply(1:N, function(j) beta0[j] + beta1[j])                        ## B1 level
-  gamma2 = sapply(1:N, function(j) beta0[j] + beta1[j] + beta2[j])             ## A2 level
-  gamma3 = sapply(1:N, function(j) beta0[j] + beta1[j] + beta2[j] + beta3[j])  ## B2 level
+  beta0A1 = beta0                                                               ## A1 level
+  beta0B1 = sapply(1:N, function(j) beta0[j] + beta1[j])                        ## B1 level
+  beta0A2 = sapply(1:N, function(j) beta0[j] + beta1[j] + beta2[j])             ## A2 level
+  beta0B2 = sapply(1:N, function(j) beta0[j] + beta1[j] + beta2[j] + beta3[j])  ## B2 level
+  sigma =   sapply(1:N, function(j) 1/sqrt(tau[j]))                             ## stdev
 
-  return(data.frame(gamma0, gamma1, gamma2, gamma3))
+  return(data.frame(beta0A1, beta0B1, beta0A2, beta0B2, sigma))
 
 }
-
-
-
