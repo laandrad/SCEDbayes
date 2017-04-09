52a4922b7671208241a57e70ecc5ff0994e0a245
Alejandro Andrade <landrad78@gmail.com>
2017-04-09 10:01:59 -0600

    Update delta estimation.R

--- R/delta estimation.R
+++ R/delta estimation.R
@@ -16,8 +16,9 @@ compute.delta <- function(y, P, s, bayes.coeff, model) {
   nPoints = length(P)
   x = P[-1] - P[-nPoints]
   nPhase = which(!x==0)
-  nPhase = c(nPhase, nPoints)
-  nPhase = c(nPhase[1], nPhase[-1] - nPhase[-4])
+  lPhase = c(0,nPhase, nPoints)
+  lPhase = lPhase[-1] - lPhase[-length(lPhase)]
+  lPhase = (lPhase-1) / 2
 
   if(model == 'trend'){
 
@@ -43,18 +44,14 @@ compute.delta <- function(y, P, s, bayes.coeff, model) {
     })
 
     ## calculate effect sizes
-    beta1.star = sapply(1:N, function(j) beta1[j] - beta5[j]*nPhase[1] )          ## B1 level
-    beta2.star = sapply(1:N, function(j) beta2[j] - beta6[j]*nPhase[2] )          ## A2 level
-    beta3.star = sapply(1:N, function(j) beta3[j] - beta7[j]*nPhase[3] )          ## B2 level
-
-    delta1 = sapply(1:N, function(j) beta1.star[j] + beta5[j] * (nPhase[2]-1)/2 ) ## A1B1
-    delta2 = sapply(1:N, function(j) beta2.star[j] + beta6[j] * (nPhase[3]-1)/2 ) ## B1A2
-    delta3 = sapply(1:N, function(j) beta3.star[j] + beta7[j] * (nPhase[4]-1)/2 ) ## A2B2
+    delta1 = sapply(1:N, function(j) beta1[j] - beta5[j]*lPhase[1] )          ## A1B1
+    delta2 = sapply(1:N, function(j) beta2[j] - beta6[j]*lPhase[2] )          ## B1A2
+    delta3 = sapply(1:N, function(j) beta3[j] - beta7[j]*lPhase[3] )          ## A2B2
 
     ## standardize effect sizes
-    delta1 = sapply(1:N, function(j) delta1[j] / stdev[j])                        ## A1B1
-    delta2 = sapply(1:N, function(j) delta2[j] / stdev[j])                        ## B1A2
-    delta3 = sapply(1:N, function(j) delta3[j] / stdev[j])                        ## A2B2
+    delta1 = sapply(1:N, function(j) delta1[j] / stdev[j])                    ## A1B1
+    delta2 = sapply(1:N, function(j) delta2[j] / stdev[j])                    ## B1A2
+    delta3 = sapply(1:N, function(j) delta3[j] / stdev[j])                    ## A2B2
 
     return(data.frame(delta1, delta2, delta3))
 
